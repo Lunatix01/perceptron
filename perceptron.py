cb3dcdb684566c40676816ebdf42cd6dec76fda7
LunatiX <56197620+Lunatix01@users.noreply.github.com>
2022-12-02 22:41:13 +0300

    added comments

--- perceptron.py
+++ perceptron.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# add function that adds two (1*2) matrixes
+
 
 def add(w0, t, x):
     print("add")
     w1 = w0[0] + x[0]
     w2 = w0[1] + x[1]
     t = t+1
-    print("w1: ", w1)
-    print("w2: ", w2)
     return list([w1, w2])
+
+# add subtract that subtract two (1*2) matrixes
 
 
 def subtract(w0, t, x):
@@ -17,23 +19,29 @@ def subtract(w0, t, x):
     w1 = w0[0] - x[0]
     w2 = w0[1] - x[1]
     t = t+1
-    print("w1: ", w1)
-    print("w2: ", w2)
     return list([w1, w2])
 
 
+# count used for plotting either red point or blue (odd count = blue)
 count = 0
+# initial weight from -.5 , .5
 w0 = np.random.uniform(-.5, .5, 2)
-print("inital w: ", w0)
+# initializing data which has Class A and B, X's and Labels
 data = np.zeros(shape=(1, 3), dtype=object)
+# find slope
 perpendecular = [w0[1], w0[0]*-1]
 slope = perpendecular[1]/perpendecular[0]
+# create 100 random points to plot with slope
 x = np.linspace(-20.1, 20.1, 100)
 b = 0
+# y = mx + b
 y = slope*x+b
+# plot the line
 ln, = plt.plot(x, y, 'k-')
 
-for t in range(0, 100):
+# plot the points and seperate them by with a line
+for t in range(0, 30):
+    # generate a random value in either class A or B
     if (count % 2 == 0):
         x1 = np.random.randint(0.1, 20)
         y1 = np.random.randint(-10.1, 0.1)
@@ -50,38 +58,43 @@ for t in range(0, 100):
         plt.plot([x2], [y2], 'bo')
         plt.pause(0.5)
         count = count+1
+    # the algorithm conditions
     x = data[len(data)-1]
     if (x[2] == "red" and np.dot(w0, [x[0], x[1]]) > 0):
         print("skip1")
         continue
     if (x[2] == "red" and np.dot(w0, [x[0], x[1]]) <= 0):
-        print("add")
+        # get new weight and find slope with [0,0] and new weight to plot
         w0 = add(w0, t, [x[0], x[1]])
         ln.remove()
-        line = np.linspace(w0[1], 0, 100)
-        line2 = np.linspace(w0[0]*-1, 0, 100)
+        # find slope
         perpendecular = [w0[1], w0[0]*-1]
         slope = perpendecular[1]/perpendecular[0]
         x = np.linspace(-20.1, 20.1, 100)
         b = 0
         y = slope*x+b
+        # plot the line
         ln, = plt.plot(x, y, 'k-')
+        # pause to see the line
         plt.pause(0.5)
     if (x[2] == "blue" and np.dot(w0, [x[0], x[1]]) < 0):
         print("skip2")
         continue
     if (x[2] == "blue" and np.dot(w0, [x[0], x[1]]) >= 0):
-        print("subtract")
+        # get new weight and find slope with [0,0] and new weight to plot
         w0 = subtract(w0, t, [x[0], x[1]])
+        # remove older line
         ln.remove()
+        # find slope
         perpendecular = [w0[1], w0[0]*-1]
         slope = perpendecular[1]/perpendecular[0]
         x = np.linspace(-20.1, 20.1, 100)
         b = 0
         y = slope*x+b
+        # plot the line
         ln, = plt.plot(x, y, 'k-')
+        # pause to see the line
         plt.pause(0.5)
-
-
+# title and show the plot
 plt.title('Perceptron')
 plt.show()
